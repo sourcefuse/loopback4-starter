e39bd3bb13059fa732924bc3af916febed677049
Samarpan <samarpan.bhattacharya@sourcefuse.com>
2019-05-06 14:36:47 +0530

    env vars fixes

--- src/application.ts
+++ src/application.ts
@@ -1,15 +1,17 @@
 import {BootMixin} from '@loopback/boot';
 import {ApplicationConfig} from '@loopback/core';
+import {RepositoryMixin} from '@loopback/repository';
+import {RestApplication} from '@loopback/rest';
 import {
   RestExplorerBindings,
   RestExplorerComponent,
 } from '@loopback/rest-explorer';
-import {RepositoryMixin} from '@loopback/repository';
-import {RestApplication} from '@loopback/rest';
 import {ServiceMixin} from '@loopback/service-proxy';
+import * as dotenv from 'dotenv';
+import * as dotenvExt from 'dotenv-extended';
 import * as path from 'path';
+
 import {MySequence} from './sequence';
-import {SoftDeleteComponent, SoftCrudRepository} from 'loopback4-soft-delete';
 
 export class Loopback4StarterApplication extends BootMixin(
   ServiceMixin(RepositoryMixin(RestApplication)),
@@ -29,8 +31,6 @@ export class Loopback4StarterApplication extends BootMixin(
     });
     this.component(RestExplorerComponent);
 
-    this.component(SoftDeleteComponent);
-
     this.projectRoot = __dirname;
     // Customize @loopback/boot Booter Conventions here
     this.bootOptions = {
@@ -46,5 +46,11 @@ export class Loopback4StarterApplication extends BootMixin(
         nested: true,
       },
     };
+
+    dotenv.config();
+    dotenvExt.load({
+      schema: '.env.example',
+      errorOnMissing: false,
+    });
   }
 }
