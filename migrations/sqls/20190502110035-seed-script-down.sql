73d2b75e1a95fcc93f97c3e0c86a3a35338e9bd7
sherif2011 <sherif2011@users.noreply.github.com>
2021-05-19 21:51:59 -0400

    Update 20190502110035-seed-script-down.sql (#25)

--- migrations/sqls/20190502110035-seed-script-down.sql
+++ migrations/sqls/20190502110035-seed-script-down.sql
@@ -1,7 +1,16 @@
 /* Replace with your SQL commands */
-SET search_path TO lbstarter,public;
+SET search_path TO admin,public;
 
-delete from user_tenants;
-delete from users;
-delete from tenants;
+alter table user_tenants drop constraint fk_user_tenants_roles;
+alter table tenants drop constraint fk_created_by;
+alter table tenants drop constraint fk_modified_by;
+alter table users drop constraint fk_users_tenants_default_tenant;
+alter table user_tenant_permissions drop constraint fk_user_tenant_permissions;
+alter table users drop constraint fk_user_user_tenant_created_by;
+alter table users drop constraint fk_user_user_tenant_modified_by;
 delete from roles;
+delete from groups;
+delete from user_tenants;
+delete from user_tenant_permissions;
+delete from tenants;
+delete from users;
